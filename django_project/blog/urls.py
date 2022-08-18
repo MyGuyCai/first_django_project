8a6c589ec3370c25faaf7f20550248939bc277ea
MyGuyCai <myguycai@outlook.com>
2022-08-18 17:03:33 +0100

    Refactored the directories, django_project is now empty of webpages A template to use throughout the site has been applied to all webpages New app called home will handle the home page Blog will now only handle the blogs

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='blog_index'),
+    path('about_us', views.about_us, name="about_us"),
+    path('services', views.services, name="services"),
+    path('contact_us', views.contact_us, name="contact_us")
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'index.html')
+
+
+def about_us(request):
+    return render(request, "about_us.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def contact_us(request):
+    return render(request, "contact_us.html")
+
+
+
