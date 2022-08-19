ed382059ff61ddadad23f344a093d18ce03d844f
MyGuyCai <myguycai@outlook.com>
2022-08-19 12:24:55 +0100

    Installed django_plotly_dash, registered the app in settings.py, added plotly settings in settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_plotly_dash.apps.DjangoPlotlyDashConfig',
     'blog',
     'home',
     'graphing',
@@ -128,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Plotly settings
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
