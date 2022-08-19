a4a79d05bcefc6de322f7b863d57221935cdd9ce
MyGuyCai <myguycai@outlook.com>
2022-08-19 11:42:39 +0100

    Created a file upload system using FileSystemStorage in the graphics app, added MEDIA_ROOT and MEDIA_URL to settings.py Uploaded fake data to display the graph without uploading your own file

--- django_project/graphing/views.py
+++ django_project/graphing/views.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
-    return render(request, 'graph_index.html')
+    context = {}
+    if request.method == 'POST':
+        context = {}
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name, uploaded_file.size)
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+
+    return render(request, 'graph_index.html', context)
